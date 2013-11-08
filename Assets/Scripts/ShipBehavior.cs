ce610db8cd6007b6b8a78dbf78090ce1e2fa17c5
Cameron Sutter <cameronsutter0@gmail.com>
2013-11-07 23:43:26 -0700

    shields up, commander!

---
+++ Assets/Scripts/ShipBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipBehavior : MonoBehaviour {
+	
+	private GameObject shield;
+
+	void Start () {
+		shield = transform.Find("shield").gameObject;
+	}
+	
+	void Update () {
+		//check for shield up
+		if(Input.GetButtonDown("Jump")){
+			shield.SetActive(true);	
+		}
+	}
+}
