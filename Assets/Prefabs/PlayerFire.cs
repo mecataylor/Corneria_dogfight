fee07b1e76473211abd9649929c05de97f19d845
Cameron Sutter <csutter@instructure.com>
2013-11-11 09:51:42 -0700

    Bulletkill the lasers

--- Assets/Prefabs/PlayerFire.cs
+++ Assets/Prefabs/PlayerFire.cs
@@ -11,18 +11,11 @@ public class PlayerFire : MonoBehaviour
 	{
 		if(Input.GetButtonDown("Fire1"))
 		{
-			float timer = 0;
 			Vector3 shootVector = transform.position + new Vector3(1f, 0f, 1f);
 			Rigidbody newLaser = Instantiate(bullet, shootVector, transform.rotation) as Rigidbody;
 			newLaser.AddForce(transform.forward*velocity,ForceMode.VelocityChange);
-			
-			timer += 1.0f * Time.deltaTime;
-			
-			if (timer > 2.25f)
-			{
-				GameObject.Destroy(newLaser);
-			}
 		}
+		
 		
 	}
 }
