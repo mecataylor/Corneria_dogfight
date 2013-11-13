c52557cb897e8f1046457f5303a7d1074b981ab3
Cameron Sutter <cameronsutter0@gmail.com>
2013-11-13 13:31:58 -0700

    behave, ship
    Consolidated some ship scripts

--- Assets/Scripts/ShipBehavior.cs
+++ Assets/Scripts/ShipBehavior.cs
@@ -4,15 +4,77 @@ using System.Collections;
 public class ShipBehavior : MonoBehaviour {
 	
 	private GameObject shield;
+	
+	public Rigidbody bullet;
+	public float laser_velocity = 125.0f;
+		
+	public int throttle = 60;
+	public int boost = 10;
+	public float xSensitivity = 1.0f;
+	public float rollSensitivity = 5.0f;
+	public float pitchSensitivity = 1.0f;
+	public float selfRightingSpeed = 1.0f;
+	public float rotateOnYaw = 1f;
 
 	void Start () {
 		shield = transform.Find("shield").gameObject;
 	}
 	
 	void Update () {
-		//check for shield up
+		fire ();
+		shieldsUp();
+		fly();
+	}
+	
+	//fire when ready
+	void fire(){
+		if(Input.GetButtonDown("Fire1"))
+		{
+			float velocity = laser_velocity + throttle;
+			Rigidbody newLaser = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+			newLaser.AddForce(transform.forward * velocity,ForceMode.VelocityChange);
+		}		
+	}
+	
+	void shieldsUp(){
 		if(Input.GetButtonDown("Jump")){
 			shield.SetActive(true);	
+		}		
+	}
+	
+	void fly() {
+		
+		float forwardInput = Input.GetAxis("Triggers");
+		float pitchInput = Input.GetAxis("LeftJoystickY");
+		float rollInput = Input.GetAxis("RightJoystickX");
+		float yawInput = Input.GetAxis("LeftJoystickX");
+		
+		if(forwardInput < -.001){
+			rigidbody.velocity = transform.forward * throttle * (boost * -forwardInput);
+		}else{
+			rigidbody.velocity = transform.forward * throttle;
 		}
+		
+		float yaw = Time.deltaTime * throttle * yawInput * xSensitivity;
+		float roll = Time.deltaTime * throttle * rollInput * rollSensitivity;
+		float pitch = Time.deltaTime * throttle * pitchInput * pitchSensitivity;
+
+		//slight roll as you yaw
+		float xRoll = 0;
+		if(Mathf.Abs(yaw) > 0.05){
+			xRoll = Mathf.Sign(yaw) * -rotateOnYaw;
+			// This one might work for the railed version
+			//transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.rotation.x, yaw, rotateOnYaw), Time.deltaTime * 1.5f);
+		}
+		
+		transform.Rotate(0, yaw, xRoll);
+		transform.Rotate(-pitch, 0, 0);
+		transform.Rotate(0, 0, -roll);
+		
+		selfRighting();
+	}
+	
+	void selfRighting(){
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.deltaTime * selfRightingSpeed);
 	}
 }
--- Assets/Scripts/flightForces.js
+++ Assets/Scripts/flightForces.js
@@ -4,42 +4,38 @@ var boost : int = 10;
 var xSensitivity : float = 1.0;
 var rollSensitivity : float = 5.0;
 var pitchSensitivity : float = 1.0;
-
+var selfRightingSpeed : float = 1.0;
 
 
 function Update () {
     
-	var forwardInput : float = Input.GetAxis("Triggers");
-	var pitchInput : float = Input.GetAxis("LeftJoystickY");
-	var rollInput : float = Input.GetAxis("RightJoystickX");
-	var xInput : float = Input.GetAxis("LeftJoystickX");
-	
-	if(forwardInput < -.001){
-		rigidbody.velocity = transform.forward * throttle * (boost * -forwardInput);
-	}else{
-		rigidbody.velocity = transform.forward * throttle;
-	}
-	
-	var x : float = Time.deltaTime * throttle * xInput * xSensitivity;
-	var roll : float = Time.deltaTime * throttle * rollInput * rollSensitivity;
-	var pitch : float = Time.deltaTime * throttle * pitchInput * pitchSensitivity;
-	
-	var xRoll : float = 0;
-	if(Mathf.Abs(x) > 0.05){
-		xRoll = Mathf.Sign(x) * -1.5;
-	}
-	
-	transform.Rotate(0, x, xRoll);
-	transform.Rotate(-pitch, 0, 0);
-	//for roll it might be better to Lerp to a fixed position based on where their joystick is
-	// so for example, all the way to the right on the joystick is always turned 90 degrees right, no more
-	transform.Rotate(0, 0, -roll);
-     
-	//self-righting
-	//this needs some tweaking for when the ship is turned more than 180 degrees
-	if(Mathf.Abs(transform.rotation.z) > .005){
-		var right : float = Time.deltaTime * throttle * Mathf.Sign(transform.rotation.z);
-		transform.Rotate(0, 0, right);
-	}
+//	var forwardInput : float = Input.GetAxis("Triggers");
+//	var pitchInput : float = Input.GetAxis("LeftJoystickY");
+//	var rollInput : float = Input.GetAxis("RightJoystickX");
+//	var xInput : float = Input.GetAxis("LeftJoystickX");
+//	
+//	if(forwardInput < -.001){
+//		rigidbody.velocity = transform.forward * throttle * (boost * -forwardInput);
+//	}else{
+//		rigidbody.velocity = transform.forward * throttle;
+//	}
+//	
+//	var x : float = Time.deltaTime * throttle * xInput * xSensitivity;
+//	var roll : float = Time.deltaTime * throttle * rollInput * rollSensitivity;
+//	var pitch : float = Time.deltaTime * throttle * pitchInput * pitchSensitivity;
+//	
+//	var xRoll : float = 0;
+//	if(Mathf.Abs(x) > 0.05){
+//		xRoll = Mathf.Sign(x) * -1.5;
+//	}
+//	
+//	transform.Rotate(0, x, xRoll);
+//	transform.Rotate(-pitch, 0, 0);
+//	//for roll it might be better to Lerp to a fixed position based on where their joystick is
+//	// so for example, all the way to the right on the joystick is always turned 90 degrees right, no more
+//	transform.Rotate(0, 0, -roll);
+//     
+//	//self-righting
+//	transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.deltaTime * selfRightingSpeed);
 
 }
