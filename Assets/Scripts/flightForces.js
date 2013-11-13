d80691336598dd47497ade2732a775f081f62261
Cameron Sutter <cameronsutter0@gmail.com>
2013-11-12 23:11:15 -0700

    We have our heading
    Improved flight controls

--- Assets/Scripts/flightForces.js
+++ Assets/Scripts/flightForces.js
@@ -1,79 +1,45 @@
 ﻿    
-var throttle : int;                 // current value for throttle
-var maxThrottle : int;              // maximum value for throttle
-var minThrottle : int;              // minimum value for throttle
-var alieronSensitivity : float;     // how sensitive the ship is to rotation (could be adjusted according to speed)
-var selfRightingSpeed : float;      // damping speed for alieron rotation
-var elevatorSensitivity : float;    // how sensitive the ship is to up and down rotation (could be adjusted according to speed)
-var tailResponsivness : float;
-var rudderSensitivity : float;
+var throttle : int = 60;
+var boost : int = 10;
+var xSensitivity : float = 1.0;
+var rollSensitivity : float = 5.0;
+var pitchSensitivity : float = 1.0;
 
-function Start () {
 
-}
-
-function controlThrottle(){
-    // throttle controls
-    if (Input.GetButtonDown ("RightBumper")) {
-		throttle += 5;
-        if ( throttle > maxThrottle ){
-            throttle = maxThrottle;
-        }
-	}
-    if (Input.GetButtonDown ("LeftBumper")) {
-        throttle -= 5;
-        if ( throttle < minThrottle ){
-            throttle = minThrottle;
-        }
-    }
-}
-
-function controlAlierons(){
-    // set the percent throttle for making the plane more or less responsive at different speeds.
-    var throttlePercent = ((rigidbody.velocity.magnitude * 1.0) / maxThrottle);
-    
-    // alieron rotation
-    if ( Mathf.Abs( Input.GetAxis ( "LeftJoystickX" ) ) > 0.15 ) {
-        rigidbody.AddRelativeTorque( Vector3.forward * -1 * Input.GetAxis ( "LeftJoystickX" ) * alieronSensitivity * throttlePercent );
-    }
-    else {
-        rigidbody.angularVelocity = Vector3.Lerp(rigidbody.angularVelocity, Vector3.zero, Time.deltaTime * selfRightingSpeed);
-    }
-}
-
-function controlElevators(){
-    // elevator rotation
-    if ( Mathf.Abs( Input.GetAxis ( "LeftJoystickY" ) ) > 0.15 ) {
-        rigidbody.AddRelativeTorque( Vector3.left * Input.GetAxis ( "LeftJoystickY" ) * elevatorSensitivity );
-    }
-    else {
-        rigidbody.angularVelocity = Vector3.Lerp(rigidbody.angularVelocity, Vector3.zero, Time.deltaTime * tailResponsivness);
-    }
-}
-
-function controlRudder(){
-	// elevator rotation
-    if ( Mathf.Abs( Input.GetAxis ( "RightJoystickX" ) ) > 0.15 ) {
-        rigidbody.AddRelativeTorque( Vector3.up * Input.GetAxis ( "RightJoystickX" ) * rudderSensitivity );
-    }
-    else {
-        rigidbody.angularVelocity = Vector3.Lerp(rigidbody.angularVelocity, Vector3.zero, Time.deltaTime * tailResponsivness);
-    }
-}
-
-function calculateLift(){
-	rigidbody.AddRelativeForce( Vector3.up * rigidbody.velocity.magnitude * 0.25 );
-}
 
 function Update () {
-    controlThrottle();
-    controlAlierons();
-    controlElevators();
-    calculateLift();
-    controlRudder();
     
-    
-    rigidbody.AddRelativeForce( Vector3.forward*throttle );
-    
-    
+	var forwardInput : float = Input.GetAxis("Triggers");
+	var pitchInput : float = Input.GetAxis("LeftJoystickY");
+	var rollInput : float = Input.GetAxis("RightJoystickX");
+	var xInput : float = Input.GetAxis("LeftJoystickX");
+	
+	if(forwardInput < -.001){
+		rigidbody.velocity = transform.forward * throttle * (boost * -forwardInput);
+	}else{
+		rigidbody.velocity = transform.forward * throttle;
+	}
+	
+	var x : float = Time.deltaTime * throttle * xInput * xSensitivity;
+	var roll : float = Time.deltaTime * throttle * rollInput * rollSensitivity;
+	var pitch : float = Time.deltaTime * throttle * pitchInput * pitchSensitivity;
+	
+	var xRoll : float = 0;
+	if(Mathf.Abs(x) > 0.05){
+		xRoll = Mathf.Sign(x) * -1.5;
+	}
+	
+	transform.Rotate(0, x, xRoll);
+	transform.Rotate(-pitch, 0, 0);
+	//for roll it might be better to Lerp to a fixed position based on where their joystick is
+	// so for example, all the way to the right on the joystick is always turned 90 degrees right, no more
+	transform.Rotate(0, 0, -roll);
+     
+	//self-righting
+	//this needs some tweaking for when the ship is turned more than 180 degrees
+	if(Mathf.Abs(transform.rotation.z) > .005){
+		var right : float = Time.deltaTime * throttle * Mathf.Sign(transform.rotation.z);
+		transform.Rotate(0, 0, right);
+	}
+
 }
