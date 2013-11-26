55180fbf9c650b966792e4fdadfc8e5713733609
Cameron Sutter <cameronsutter0@gmail.com>
2013-11-25 23:09:54 -0700

    Added velocity to what is sent over the network

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -5,12 +5,14 @@ public class NetworkPlayer : Photon.MonoBehaviour {
 	
 	private Vector3 correctPlayerPos;
 	private Quaternion correctPlayerRot;
+	private Vector3 correctPlayerVel;
 	private bool isShooting;
 	
 	void Update(){
 		if (!photonView.isMine){
-			transform.position = Vector3.Lerp (transform.position, this.correctPlayerPos, Time.deltaTime * 5);
-			transform.rotation = Quaternion.Lerp (transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+			transform.position = Vector3.Lerp (transform.position, correctPlayerPos, Time.deltaTime * 5);
+			transform.rotation = Quaternion.Lerp (transform.rotation, correctPlayerRot, Time.deltaTime * 5);
+			rigidbody.velocity = correctPlayerVel;
 			if(isShooting){
 				transform.SendMessage("startShooting");
 			}else{
@@ -24,12 +26,14 @@ public class NetworkPlayer : Photon.MonoBehaviour {
 			//We own this player: send the others our data
 			stream.SendNext (transform.position);
 			stream.SendNext (transform.rotation);
+			stream.SendNext (rigidbody.velocity);
 			stream.SendNext (ShipBehavior.isShooting);
 		}
 		else{
 			//Network Player, receive data
-			this.correctPlayerPos = (Vector3) stream.ReceiveNext();
-			this.correctPlayerRot = (Quaternion) stream.ReceiveNext();
+			correctPlayerPos = (Vector3) stream.ReceiveNext();
+			correctPlayerRot = (Quaternion) stream.ReceiveNext();
+			correctPlayerVel = (Vector3) stream.ReceiveNext();
 			isShooting = (bool) stream.ReceiveNext();
 		}
 	}
