699cb25f6a7af4d106201901e54d0ebdc3b4313a
Cameron Sutter <cameronsutter0@gmail.com>
2014-01-27 22:14:16 -0700

    collisions over network (not working)

---
+++ Assets/shared/scripts/shieldBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shieldBehavior : MonoBehaviour {
+
+	public float duration = 0.5f;
+	public Transform explosion;
+	
+	void OnCollisionEnter(Collision col){
+		Debug.Log("Getting shot");
+		int layer = Env.enemyFireLayer;
+		if(Env.isDogFight()){
+			layer = Env.droneFireLayer;
+		}
+		if(col.gameObject.layer == layer){
+			Destroy(col.gameObject);
+			//Add an explosion at the site of the collision
+			Instantiate(explosion, col.transform.position, col.transform.rotation);
+		}
+	}
+	
+	void OnEnable(){
+		StartCoroutine(Disable(gameObject, duration));
+	}
+	
+	IEnumerator Disable(GameObject target, float duration) {
+		yield return new WaitForSeconds(duration);
+		target.SetActive(false);
+	}
+}
