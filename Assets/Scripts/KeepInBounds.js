f884ef4b269972af3aac23d59fbb3c13860348ff
Cameron Sutter <cameronsutter0@gmail.com>
2014-02-06 23:26:13 -0700

    fixed boundary behavior

--- Assets/Scripts/KeepInBounds.js
+++ Assets/Scripts/KeepInBounds.js
@@ -1,35 +1,31 @@
 ﻿#pragma strict
 
-var mapXMax : int;
-var mapXMin : int;
+//var mapXMax : int;
+//var mapXMin : int;
 var mapYMax : int;
 var mapYMin : int;
-var mapZMax : int;
-var mapZMin : int;
-
-function Start () {
-
-}
+//var mapZMax : int;
+//var mapZMin : int;
 
 function keepInBounds(){
-	if (rigidbody.transform.position.x < mapXMin ){
-		rigidbody.transform.position.x = mapXMax;
-	}
-	if (rigidbody.transform.position.x > mapXMax ){
-		rigidbody.transform.position.x = mapXMin;
-	}
+//	if (rigidbody.transform.position.x < mapXMin ){
+//		rigidbody.transform.position.x = mapXMax;
+//	}
+//	if (rigidbody.transform.position.x > mapXMax ){
+//		rigidbody.transform.position.x = mapXMin;
+//	}
 	if (rigidbody.transform.position.y < mapYMin ){
 		rigidbody.transform.position.y = mapYMin;
 	}
 	if (rigidbody.transform.position.y > mapYMax ){
 		rigidbody.transform.position.y = mapYMax;
 	}
-	if (rigidbody.transform.position.z < mapZMin ){
-		rigidbody.transform.position.z = mapZMax;
-	}
-	if (rigidbody.transform.position.z > mapZMax ){
-		rigidbody.transform.position.z = mapZMin;
-	}
+//	if (rigidbody.transform.position.z < mapZMin ){
+//		rigidbody.transform.position.z = mapZMax;
+//	}
+//	if (rigidbody.transform.position.z > mapZMax ){
+//		rigidbody.transform.position.z = mapZMin;
+//	}
 }
 
 function Update () {
